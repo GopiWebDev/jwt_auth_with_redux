e6daa85c7d3f336f8bfd14c56ff11c50010eb7ec
GopiWebDev <gopisv.webdev@gmail.com>
2024-11-17 15:50:04 +0530

    configured user routes

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,20 @@
 import express from 'express'
-const port = 5000
+import dotenv from 'dotenv'
+dotenv.config()
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
+const port = process.env.PORT || 5000
+import userRoutes from './routes/userRoutes.js'
 
 const app = express()
+
+app.use('/api/users', userRoutes)
 
 app.get('/', (req, res) => {
   res.send('Server ready')
 })
+
+app.use(notFound)
+app.use(errorHandler)
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`)
